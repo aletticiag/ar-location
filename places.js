e9f1fdb5892a498bf138a3f92a0b1e8224832e4f
alexgf <agferreira.rj@gmail.com>
2022-05-25 11:05:50 -0300

    Teste sem gps simulado

--- places.js
+++ places.js
@@ -99,7 +99,7 @@ function renderPlaces(places) {
         text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
         text.setAttribute('title', place.name);
         text.setAttribute('href', 'http://www.example.com/');
-        text.setAttribute('scale', '15 15 15');
+        text.setAttribute('scale', '30 30 30');
 
         text.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
