1dd49446309f8cfa79a66960e6fb8d99cc68cfd6
alexgf <agferreira.rj@gmail.com>
2022-06-03 14:13:53 -0300

    ajuste no teste images

--- images/index.js
+++ images/index.js
@@ -37,7 +37,7 @@ function staticLoadPlaces() {
                 lng: -43.1765680709474, // change here longitude if using static data
             },
             link: "http://theatromunicipal.rj.gov.br/",
-            img: "#theatro"
+            img: "./theatro_plateia.jpg"
         },
         {
             name: "Arcos da Lapa",
@@ -46,7 +46,7 @@ function staticLoadPlaces() {
                 lng: -43.1799963698298, // change here longitude if using static data
             },
             link: "https://freewalkertours.com/pt-br/arcos-da-lapa-rio-de-janeiro/",
-            img: "#arcos-da-lapa"
+            img: "Arcos_da_Lapa.jpg"
         }
     ];
 }
@@ -103,13 +103,38 @@ function renderPlaces(places) {
         let imagem = document.createElement('a-image')
         imagem.setAttribute('gps-projected-entity-place', {latitude,longitude});
         imagem.setAttribute('look-at', '[gps-projected-camera]');
-        imagem.setAttribute('scale', '15 15 15');
         imagem.setAttribute('src', place.img)
+        imagem.setAttribute('scale', '20, 20');
 
         imagem.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-projected-entity-place-loaded', { detail: { component: this.el }}))
         });
 
+        const clickListener = function (ev) {
+            ev.stopPropagation();
+            ev.preventDefault();
+
+            const name = ev.target.getAttribute('name');
+
+            const el = ev.detail.intersection && ev.detail.intersection.object.el;
+
+            if (el && el === ev.target) {
+                const label = document.createElement('span');
+                const container = document.createElement('div');
+                container.setAttribute('id', 'place-label');
+                label.innerText = name;
+                container.appendChild(label);
+                document.body.appendChild(container);
+
+                setTimeout(() => {
+                    container.parentElement.removeChild(container);
+                }, 1500);
+            }
+        };
+
+        imagem.addEventListener('click', clickListener);
+
         scene.appendChild(imagem);
     });
+
 }
